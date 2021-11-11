c78fff793ca4cc6b02c48cb86ece6b9e4bd36ee1
Ashley Savage <chavon1189@gmail.com>
2021-11-10 22:07:26 -0500

    begins  adding css and js for messages page

--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -1,3 +1,22 @@
+import React from "react";
+import MessagesList from "./MessagesList";
+import MessagesContent from "./MessagesContent";
+import MessagesUser from "./MessagesUser";
+
+class Messages extends React.Component {
+    render() {
+        return (
+            <div className="message">
+                <MessagesList/>
+                <MessagesContent/>
+                <MessagesUser/>
+            </div>
+        )
+    }
+} 
+
+export default Messages
+
 /**"Messages"
  * Search bar
  * List of messages with scroll
---
+++ src/Components/MessagesContent.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class MessagesContent extends React.Component {
+    render() {
+        return (
+            <div className="messageContent">
+                
+            </div>
+        )
+    }
+} 
+
+export default MessagesContent
