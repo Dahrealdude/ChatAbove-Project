1c54b54fef5a3d4f6926b65c3c0862a2a9d59f6e
Ashley Savage <chavon1189@gmail.com>
2021-11-11 20:18:33 -0500

    creates groups and new contact pages

--- src/Components/Groups.js
+++ src/Components/Groups.js
@@ -1,3 +1,54 @@
+import React from "react";
+import { setDoc, doc } from "firebase/firestore/lite";
+
+class Groups extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            groupName: "",
+            groupMember: "",
+        }
+        this._updateGroupName = this._updateGroupName.bind(this);
+        this._updateGroupMember = this._updateGroupMember.bind(this)
+    
+    }
+    render() {
+        return(
+            <div>
+                <div>
+                    <input type="text" value={this.state.groupName} onChange={this._updateGroupName} placeholder="Name your group"/>
+                </div>
+                <div>
+                    <input type="text" value={this.state.groupMember} onChange={this._updateGroupMember} placeholder="Add new member"/>
+                </div>
+                <p>{this.state.groupName}</p>
+                <p>{this.state.groupMember}</p>
+                <button onClick={this._addGroup}>Add Group</button>
+            </div>
+        )
+    }
+    _updateGroupName(event) {
+        this.setState({groupName: event.target.value})
+    }
+    _updateGroupMember(event) {
+        this.setState({groupMember:event.target.value})
+    }
+    _addGroup() {
+        setDoc(doc(this.props.DB, "groups", this.state.groupName), {
+            groupName:this.state.groupName,
+            groupMember: this.state.groupMember,
+            groups:[]
+        });
+        this.setState({
+            groupName: "",
+            groupMember: "",
+        })
+    }
+}
+
+export default Groups
+
 /**"Groups"
  * Search bar
  * List of groups with scroll
