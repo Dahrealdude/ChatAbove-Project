bd76c8160244d7027e3a1b1944aaec29b911aac4
Ashley Peart <peart.ashley@gmail>
2021-11-18 11:46:18 -0500

    More updates to pages

--- src/Components/Groups.js
+++ src/Components/Groups.js
@@ -1,50 +1,31 @@
 import React from "react";
-import { setDoc, doc } from "firebase/firestore/lite";
 import {
     Link,
     Outlet
   } from 'react-router-dom';
 
 
-class Groups extends React.Component {
-    
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            groupName: "",
-            groupMember: "",
-            memberNumber: "",
-        }
-
-        this._updateGroupName = this._updateGroupName.bind(this);
-        this._updateGroupMember = this._updateGroupMember.bind(this);
-        this._updateMemberNumber = this._updateMemberNumber.bind(this);
-        this._searchGroup = this._searchGroup.bind(this);
-    
-    }
-
-    render() {
-        return(
+  export default function Groups() {
+    return (
             
+        <div>
             <div>
-                <div>
-                    <div className='header'>
-                        <h1>A Chat Above</h1>
-                    </div>
+                <div className='header'>
+                    <h1>A Chat Above</h1>
+                </div>
                 
-                    <nav className='topnav'>
+                <nav className='topnav'>
 
-                        <ul>
-                            <Link to="/home">Home</Link>
-                            <Link to="/dashboard">Dashboard</Link>
-                            <Link to="/contacts">Contacts</Link>
-                            <Link to="/messages">Messages</Link>
-                        </ul>
+                    <ul>
+                        <Link to="/home">Home</Link>
+                        <Link to="/dashboard">Dashboard</Link>
+                        <Link to="/contacts">Contacts</Link>
+                        <Link to="/messages">Messages</Link>
+                    </ul>
 
-                    </nav>
+                </nav>
                     
-                    <Outlet/>
+                <Outlet/>
                 </div>
                 
                 <h1>Groups</h1>
@@ -53,77 +34,17 @@ class Groups extends React.Component {
                     <div class="leftcolumn">
 
                         <div class="space">
-                            
-                        </div>
+
                     </div>
-
-                </div>
-
-
-                <div className="nav">
-
-                    <form className="form">
-                        <div className="groupname">
-                            <input type="text" value={this.state.groupName} onChange={this._updateGroupName} placeholder="Enter your group name"/>
-                        </div>
-
-                        <div className="groupmember">
-                            <input type="text" value={this.state.groupMember} onChange={this._updateGroupMember} placeholder="Enter member name"/>
-                        </div>
-
-                        <div className="membernumber">
-                            <input type="text" value={this.state.memberNumber} onChange={this._updateMemberNumber} placeholder="Add member number"/>
-                        </div>
-
-                        <p>{this.state.groupName}</p>
-                        <p>{this.state.groupMember}</p>
-                        <p>{this.state.memberNumber}</p>
-
-                        <button className="button"  onClick={this._searchGroup}>Search Group</button>
-                    </form>
-                    
                 </div>
 
             </div>
-        )
-    }
 
-    _updateGroupName(event) {
-        this.setState({groupName: event.target.value})
-    }
 
-    _updateGroupMember(event) {
-        this.setState({groupMember: event.target.value})
-    }
-
-    _updateMemberNumber(event) {
-        this.setState({memberNumber: event.target.value})
-    }
-
-  
-    _searchGroup() {
-
-        setDoc(doc(this.props.DB, "Groups", this.state.groupName), {
-            groupName:this.state.groupName,
-            groupMember: this.state.groupMember,
-            memberNumber:this.state.memberNumber,
-            groups:[]
-        });
-
-        this.setState({
-            groupName: "",
-            groupMember: "",
-            memberNumber: "",
-        })
-    }
+        </div>
+    )
 }
 
-export default Groups
-
-
 /**"Groups"
- * Search bar
- * List of groups with scroll
  * Div with an image, timestamp, group name, message
- *  
 */
--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -6,36 +6,37 @@ import {
 
   export default function Messages() {
     return (
+        <div>
             <div>
-                <div>
-                    <div className='header'>
-                        <h1>A Chat Above</h1>
-                    </div>
-                
-                    <nav className='topnav'>
-
-                        <ul>
-                            <Link to="/home">Home</Link>
-                            <Link to="/dashboard">Dashboard</Link>
-                            <Link to="/contacts">Contacts</Link>
-                            <Link to="/groups">Groups</Link>
-                        </ul>
-                        
-                    </nav>
-                    
-                    <Outlet/>
+                <div className='header'>
+                    <h1>A Chat Above</h1>
                 </div>
-                <div class="row">
+                
+                <nav className='topnav'>
 
-                    <div class="leftcolumn">
+                    <ul>
+                        <Link to="/home">Home</Link>
+                        <Link to="/dashboard">Dashboard</Link>
+                        <Link to="/contacts">Contacts</Link>
+                        <Link to="/groups">Groups</Link>
+                    </ul>
+                        
+                </nav>
+                    
+                <Outlet/>
+            </div>
+            <h1>Messages</h1>
+            <div class="row">
 
-                        <div class="space">
-                        </div>
+                <div class="leftcolumn">
+
+                    <div class="space">
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
 
 /**"Messages"
  * Div with an image, timestamp, contact name, list of messages 
